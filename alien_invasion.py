d97576e5a44aee8dc7bc0c964fad410d8b8830ff
AKnox12 <aknox12@student.cscc.edu>
2025-04-06 21:11:04 -0400

    Created a Settings class and change BG color to be a different color.

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,27 +1,47 @@
 import sys
 import pygame
 
+from settings import Settings
+
 class AlienInvasion:
+    '''Overall class, manages game assets and behavior.'''
 
     def __init__(self) -> None:
+        '''Initialize the game, and create game resources.'''
         pygame.init()
+
+        self.clock = pygame.time.Clock()
+        self.settings = Settings()
+
+        self.screen = pygame.display.set_mode(
+            (self.settings.screen_width, self.settings.screen_height))
+        
 
         self.screen = pygame.display.set_mode((1200,800))
         pygame.display.set_caption("Alien Invasion")
 
+        # Set background color.
+        self.bg_color = (230,230,230)
+
         self.running = True
 
     def run_game(self) -> None:
-        # Game Loop
+        '''Start of Game Loop.'''
         while self.running:
+            # Notice for keyboard and mouse events.
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     self.running = False
                     pygame.quit()
                     sys.exit()
+            # Redraw the screen during each pass through the loop.
+            self.screen.fill(self.settings.bg_color)
 
+            # Most recently drawn screen visible
             pygame.display.flip()
+            self.clock.tick(60)
 
 if __name__ == '__main__':
+    # Make a game instance, and run the game.
     ai = AlienInvasion()
     ai.run_game()
