b63b3d4978b53f9fcbbe281f3b484eed067587a0
AKnox12 <aknox12@student.cscc.edu>
2025-04-12 18:46:19 -0400

    Created and changed the sounds.

--- settings.py
+++ settings.py
@@ -18,7 +18,7 @@ class Settings:
         self.carts_speed = 5
 
         self.bullet_file = Path.cwd() / 'unit_11_alien_Invasion_starter' / 'Assets' / 'images' / 'laser_beams.png'
-        self.laser_sound = Path.cwd() / 'unit_11_alien_Invasion_starter' / 'Assets' / 'sound' / 'laser.mp3'
+        self.laser_sound = Path.cwd() / 'unit_11_alien_Invasion_starter' / 'Assets' / 'sound' / 'Beam_sound.mp3'
         self.bullet_speed = 7
         self.bullet_width = 25
         self.bullet_height = 80
